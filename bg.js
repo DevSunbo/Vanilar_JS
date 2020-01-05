34f85e637402ac640025c7ab1d4f2934f42783ec
DevSunbo <cowwhite1008@gmail.com>
2020-01-06 00:19:13 +0900

    delete unuseful function

--- bg.js
+++ bg.js
@@ -2,10 +2,6 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
-function handleImgLoad(){
-    con
-}
-
 function paintImage(imgNumber){
     const image = new Image();
     image.src = `images/${imgNumber + 1}.jpg`;
