d40532f9e2dd1e5f711290718da8faf267a02b71
DevSunbo <cowwhite1008@gmail.com>
2020-01-08 23:32:32 +0900

    날짜 추가 & 날짜와 시간 사이의 margin 없앰

--- clock.js
+++ clock.js
@@ -1,10 +1,14 @@
 const clockContainer = document.querySelector(".js-clock"),
-      clockTitle = clockContainer.querySelector("h1");
+      clockTitle = clockContainer.querySelector("h2"),
+      dateTitle = clockContainer.querySelector("h1");
 function getTime(){
   const date = new Date();
+  const months = date.getMonth();
+  const dates = date.getDate();
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
+  dateTitle.innerText = `${months+1}월 ${dates}일`;
   clockTitle.innerText = `${
     hours < 10 ? `0${hours}` : hours}:${
       minutes < 10 ? `0${minutes}` : minutes}:${
