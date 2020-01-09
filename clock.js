a823e8ff838b8ee80dbae30ded8550c102044510
DevSunbo <cowwhite1008@gmail.com>
2020-01-09 13:02:42 +0900

    change datatime type 맞게 수정

--- clock.js
+++ clock.js
@@ -3,17 +3,31 @@ const clockContainer = document.querySelector(".js-clock"),
       dateTitle = clockContainer.querySelector("h1");
 function getTime(){
   const date = new Date();
+  const years = date.getFullYear();
   const months = date.getMonth();
   const dates = date.getDate();
+  const days = date.getDay();
+  const currentDay = getDays(days);
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  dateTitle.innerText = `${months+1}월 ${dates}일`;
+  dateTitle.innerText = `${years} / ${months+1} / ${dates}`;
   clockTitle.innerText = `${
     hours < 10 ? `0${hours}` : hours}:${
       minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds}`;
+        seconds < 10 ? `0${seconds}` : seconds} (${currentDay})`
+}
 
+function getDays(days){
+  let DAY;
+  if(days === 0){DAY = `Sun`}
+  else if(days === 1) {DAY = `Mon`}
+  else if(days === 2) {DAY = `Teu`}
+  else if(days === 3) {DAY = `Wed`}
+  else if(days === 4) {DAY = 'Thu'}
+  else if(days === 5) {DAY = `Fri`}
+  else if(days === 6) {DAY = `Sat`}
+  return DAY;
 }
 
 function init(){
